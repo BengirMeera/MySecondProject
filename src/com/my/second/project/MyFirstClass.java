4e43a7d5850438fa3f053b7959372618fea78cf6
BengirMeera <miroremage.6667@gmail.com>
2020-01-28 22:15:53 -0500

    My first commit

---
+++ src/com/my/second/project/MyFirstClass.java
@@ -0,0 +1,24 @@
+package com.my.second.project;
+
+public class MyFirstClass {
+		String name;
+		static int age=31;
+		boolean isMarried;
+		static double height=5.5;
+		static String hairColor="black";
+		char gender;
+		
+		public static void totalOfAgeAndHeight() {System.out.println(age+height);}
+		
+
+		public static void main(String[] args) {
+			age=31;
+			
+		        
+			System.out.println(hairColor);
+			totalOfAgeAndHeight();
+			
+			
+			}
+	
+}
